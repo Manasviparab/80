81c5f669d91ad9755d2c63ec07d24bac93888c21
Manasviparab <78352947+Manasviparab@users.noreply.github.com>
2021-10-02 14:47:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,12 +3,11 @@ function submit()
 {
     var display_student_array = [];
 
-    for (var j = 1; j <=1; j++)
-    {
-        var name_of_the_students = document.getElementById("name_of_the_student_"+j).value;
+
+        var name_of_the_students = document.getElementById("name1").value;
         console.log(name_of_the_students);
         name_of_the_student.push(name_of_the_students);
-    }
+    
 
     console.log(name_of_the_student);
 
@@ -22,7 +21,7 @@ function submit()
     }
     var remove_commas= display_student_array.join(" ");
     console.log(remove_commas);
-    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
+    document.getElementById("display_name").innerHTML = remove_commas;
 
     document.getElementById("submit_button").style.display="none";
     document.getElementById("sort_button").style.display="inline-block";
@@ -48,6 +47,14 @@ function sorting()
 
     var remove_commas= display_student_array_sorting.join(" ");
     console.log(remove_commas);
-    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
+    document.getElementById("display_name").innerHTML = remove_commas;
 
+}
+
+function show() 
+{ 
+    var i= name_of_the_student.join("");
+     console.log(name_of_the_student); 
+     document.getElementById("p1").innerHTML=i.toString();
+      document.getElementById("sort_button").style.display="block"; 
 }
